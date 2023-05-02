b20129598af0f27931260cceff915b16ca93bc9b
o.bavykin <invisible6462@gmail.com>
2023-05-02 23:21:26 +0600

    Fix PartyPlayerCrossRef cross-reference table

--- data/src/main/java/com/inviz/data/entity/PartyPlayerCrossRef.kt
+++ data/src/main/java/com/inviz/data/entity/PartyPlayerCrossRef.kt
@@ -2,15 +2,13 @@ package com.inviz.data.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity
+@Entity(primaryKeys = ["party_id", "player_id"])
 data class PartyPlayerCrossRef(
 
     @ColumnInfo(name = "party_id")
     val partyId: String,
 
-    @PrimaryKey
     @ColumnInfo(name = "player_id")
     val playerId: String
 )
